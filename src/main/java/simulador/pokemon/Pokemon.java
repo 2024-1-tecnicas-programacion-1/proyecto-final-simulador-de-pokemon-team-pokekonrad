1d30cb25335d6e01390c5c4c4178b396ed0c1bbc
sebastian <juansega96@gmail.com>
2024-05-28 14:52:01 -0500

    Se creó la clase Pokémon
    se creo la plantilla de la clase Pokémon y sus respectivas funciones, pero falta desarrollar cada una.

--- src/main/java/simulador/pokemon/Pokemon.java
+++ src/main/java/simulador/pokemon/Pokemon.java
@@ -1,5 +1,30 @@
 package simulador.pokemon;
 
-public class Pokemon {
-    // TODO: Aquí va tu código
+public abstract class Pokemon {
+    // TODO: Aquí va tu código 
+    
+    private String nombre;
+    private double salud;
+    private double puntosDeAtaque;
+    private String tipo;
+    private String estado;
+    
+    public Pokemon(String nombre, double salud, double puntosDeAtaque, String tipo, String estado){
+        this.nombre=nombre;
+        this.salud=salud;
+        this.puntosDeAtaque=puntosDeAtaque;
+        this.tipo=tipo;
+        this.estado=estado;
+    }
+    public void atacar(Pokemon oponente){
+        
+    }
+    public void recibirDaño(int daño){
+        
+    }
+    public void entrenar(){
+        
+    }
+    
 }
+
