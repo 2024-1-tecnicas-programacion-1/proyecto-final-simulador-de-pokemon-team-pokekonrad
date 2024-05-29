7f17ea80743f0bcd3b17105d1cb234df66461173
Juandhaaaa <Sanabriacamposjuan@gmail.com>
2024-05-29 15:43:32 -0500

    Clase batalla
    Se crea la clase batalla y se desarrolla el metodo iniciar batalla, con un while que comprueba que la salud de ambos pokemones sea mayo a 0 para continuar la batalla.

--- src/main/java/simulador/batalla/Batalla.java
+++ src/main/java/simulador/batalla/Batalla.java
@@ -1,5 +1,35 @@
 package simulador.batalla;
+import simulador.pokemon.Pokemon;
+import java.util.*;
 
 public class Batalla {
-    // TODO: Aquí va tu código
+    Random nAleatorio = new Random();
+    public Pokemon inciarBatalla(Pokemon  pokemon1, Pokemon pokemon2){
+        int saludPokemon1 = 0;
+        int saludPokemon2 = 0;
+        Pokemon ganador;
+        boolean primerAtaque = nAleatorio.nextBoolean();
+        while (saludPokemon1 >0 && saludPokemon2>0) {
+            if (primerAtaque){
+                pokemon1.atacar(pokemon2);
+            }
+            else {
+                pokemon2.atacar(pokemon1);
+            }
+            if (saludPokemon1<0){
+                saludPokemon1 = 0;
+            }
+            if (saludPokemon2<0){
+                saludPokemon2 = 0;
+            }
+        }
+        if (saludPokemon1 > saludPokemon2){
+            ganador = pokemon1;
+        }
+        else{
+            ganador = pokemon2;
+        }
+        System.out.println("Gano el pokemon " + ganador.getNombre());
+        return ganador;
+    }
 }
