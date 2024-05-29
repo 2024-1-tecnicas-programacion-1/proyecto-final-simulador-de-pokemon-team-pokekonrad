85568b9a25b45f5ed2f00de36eaae3589f8b1d08
Daniel Felipe Chávez González <danielf.chavezg@konradlorenz.edu.co>
2024-05-29 12:37:06 -0500

    Se creó la clase Entrenador
    Se agregaron los métodos base de la clase Entrenador, y se creó una lista de Pokemones por entrenador

--- src/main/java/simulador/entrenador/Entrenador.java
+++ src/main/java/simulador/entrenador/Entrenador.java
@@ -1,5 +1,43 @@
 package simulador.entrenador;
+import simulador.pokemon.Pokemon;
+import java.util.*;
 
 public class Entrenador {
-    // TODO: Aquí va tu código
+    private String nombre;
+    private ArrayList<Pokemon> listaDePokemones = new ArrayList<>();
+    
+    public Entrenador(String nombre){
+        this.nombre = nombre;
+       
+    }
+    public void agregarPokemon(Pokemon pokemon){
+        listaDePokemones.add(pokemon);
+        //Añade un pokemon al equipo del entrenador
+    }
+    public void entrenarPokemones(Pokemon pokemon){
+        
+        //Selecciona un pokemon para entrenar, mejorando sus estadisticas
+    }
+    public ArrayList mostrarPokemones(){
+        return listaDePokemones;
+        //Muestra la lista de los pokemones actuales del entrenador
+    }
+    public void prepararBatalla(){
+        //Selecciona un pokemon de su equipo para participar en una batalla
+    }
+    public String getNombre(){
+        return nombre;
+    }
+
+    public ArrayList<Pokemon> getListaDePokemones() {
+        return listaDePokemones;
+    }
+
+    public void setListaDePokemones(ArrayList<Pokemon> listaDePokemones) {
+        this.listaDePokemones = listaDePokemones;
+    }
+    
+    
+
+
 }
